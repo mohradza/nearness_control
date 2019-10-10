ccb7f18bd00801b0dff5ef54fc92be7731ad8447
mohradza <mioh0887@colorado.edu>
2019-10-09 18:23:35 -0600

    edited scan visualizers

--- scripts/h_scan_visualizer.py
+++ scripts/h_scan_visualizer.py
@@ -18,12 +18,16 @@ preturn_thresh = 5.0
 plotCarts     =   1
 
 # ~~ Variables ~~
-global lastDepthScan
-global lastNearnessScan
+global lastHDepthScan
+global lastHNearnessScan
+global lastVDepthScan
+global lastHNearnessScan
 global a_0, a_1, a_2, b_1, b_2
 global forward_speed, yaw_rate
-lastDepthScan = [ 0 for i in range( numReadings ) ]
-lastNearnessScan = [ 0 for i in range( numReadings ) ]
+lastHDepthScan = [ 0 for i in range( numReadings ) ]
+lastHNearnessScan = [ 0 for i in range( numReadings ) ]
+lastVDepthScan = [ 0 for i in range( numReadings ) ]
+lastVNearnessScan = [ 0 for i in range( numReadings ) ]
 lastWFReconScan = [ 0 for i in range( numReadings ) ]
 lastSFScan = [ 0 for i in range( numReadings ) ]
 
@@ -34,83 +38,88 @@ len_hist = 500
 
 
 
-def depth_scan_cb( msg ):
-    global lastDepthScan
+def h_depth_scan_cb( msg ):
+    global lastHDepthScan
     """ Process the laser scan message """
-    lastDepthScan = msg.data  #lastDepthLaserScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
+    lastHDepthScan = msg.data  #lastDepthLaserScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
 
-def nearness_cb( msg ):
-    global lastNearnessScan
+def h_nearness_cb( msg ):
+    global lastHNearnessScan
     """ Process the nearness array """
-    lastNearnessScan = msg.data  #lastDepthImageScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
+    lastHNearnessScan = msg.data  #lastDepthImageScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
 
-def WF_recon_cb( msg ):
-    global lastWFReconScan
+def v_depth_scan_cb( msg ):
+    global lastVDepthScan
     """ Process the nearness array """
-    lastWFReconScan = msg.data  #lastDepthImageScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
+    lastVDepthScan = msg.data  #lastDepthImageScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
 
-def SF_cb( msg ):
-    global lastSFScan
+def v_nearness_cb( msg ):
+    global lastVNearnessScan
     """ Process the nearness array """
-    lastSFScan = msg.data  #lastDepthImageScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
+    lastVNearnessScan = msg.data  #lastDepthImageScan = [ elem/25.50 for elem in msg.data ] # scale [0,255] to [0,10]
 
 minAng = -pi
 maxAng = pi
 rospy.init_node( 'scan_plot' , anonymous = True )
 
 # rospy.Subscriber( "wfi/horiz/image_scan" , Float32MultiArray , image_scan_cb )
-rospy.Subscriber( "/nearness_controller/horiz_depth_reformat" , Float32MultiArray , depth_scan_cb )
-rospy.Subscriber( "/nearness_controller/horiz_nearness" , Float32MultiArray , nearness_cb )
-rospy.Subscriber( "/sf/horiz/sf_nearness" , Float32MultiArray , SF_cb )
-rospy.Subscriber( "/sf/horiz/recon_wf_nearness" , Float32MultiArray , WF_recon_cb )
+rospy.Subscriber( "/nearness_controller/horiz_depth_reformat" , Float32MultiArray , h_depth_scan_cb )
+rospy.Subscriber( "/nearness_controller/horiz_nearness" , Float32MultiArray , h_nearness_cb )
+rospy.Subscriber( "/nearness_controller/vert_depth_reformat" , Float32MultiArray , v_depth_scan_cb )
+rospy.Subscriber( "/nearness_controller/vert_nearness" , Float32MultiArray , v_nearness_cb )
 
 try:
-	while ( not rospy.is_shutdown() ):
-                lastDepthScanNP   = np.asarray( lastDepthScan )
-		lastNearnessScanNP   = np.asarray( lastNearnessScan )
-		lastWFReconScanNP   = np.asarray( lastWFReconScan )
-		lastSFScanNP   = np.asarray( lastSFScan )
+    while ( not rospy.is_shutdown() ):
 
-		plt.clf() # Clear all figures
+        lastHDepthScanNP = np.asarray(lastHDepthScan)
+        lastHNearnessScanNP   = np.asarray( lastHNearnessScan )
+        lastVDepthScanNP   = np.asarray( lastVDepthScan )
+        lastVNearnessScanNP   = np.asarray( lastVNearnessScan )
 
-		plt.figure(1)
-		plt.subplot(3,1,1)
-		# Horizontal Depth Scan: Array Index vs. Depth [m]
-		# plt.figure(num=1, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
-		plt.plot( lastDepthScanNP , 'b.' )
-		plt.hold( False )
-		plt.xlim( [ 0 , 720 ] )
-		plt.ylim( [ 0 , 15 ] )
-		plt.xlabel("Array Index")
-		plt.ylabel("Depth [m]")
-		plt.title("Horiz. Depth (Array Values)")
+        plt.figure(1)
+        plt.clf() # Clear all figures
+        plt.subplot(4,1,1)
+        # Horizontal Depth Scan: Array Index vs. Depth [m]
+        # plt.figure(num=1, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
+        plt.plot( lastHDepthScanNP , 'r.' )
+        plt.hold( False )
+        plt.xlim( [ 0 , 720 ] )
+        plt.ylim( [ 0 , 10 ] )
+        plt.xlabel("Index")
+        plt.ylabel("Depth")
 
-		plt.subplot(3,1,2)
-		# Horizontal Depth Scan: X [m] vs Y [m]
-		# plt.figure(num=2, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
-		plt.plot( lastNearnessScanNP , 'b.' )
-		plt.hold( True )
-                plt.plot(lastWFReconScanNP , 'r.' )
-		plt.hold( False )
+        plt.subplot(4,1,2)
+        # Horizontal Depth Scan: X [m] vs Y [m]
+        # plt.figure(num=2, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
+        plt.plot( lastHNearnessScanNP , 'b.' )
+        plt.hold( False )
+        plt.xlim( [ 0 , 720 ] )
+        plt.ylim( [ 0 , 5 ] )
+        plt.xlabel("Index")
+        plt.ylabel("Nearness")
 
-		plt.xlim( [ 0 , 720 ] )
-		plt.ylim( [ 0 , 2 ] )
-		plt.xlabel("Array Index")
-		plt.ylabel("Nearness [1/m]")
-		plt.title("Horiz. Nearness (Array Values)")
+        plt.subplot(4,1,3)
+        # Horizontal Nearness Scan: X [1/m] vs Y [1/m]
+        # plt.figure(num=4, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
+        plt.plot( lastVDepthScanNP , 'r.' )
+        plt.hold( False )
+        plt.xlim( [ 0 , 720 ] )
+        plt.ylim( [ 0 , 10 ] )
+        plt.xlabel("Index")
+        plt.ylabel("Depth")
 
-		plt.subplot(3,1,3)
-		# Horizontal Nearness Scan: X [1/m] vs Y [1/m]
-		# plt.figure(num=4, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
-		plt.plot( lastSFScanNP , 'b.' )
-		plt.hold( False )
-		plt.xlim( [ 0 , 720 ] )
-		plt.ylim( [ 0 , 3 ] )
-		plt.xlabel("Array Index")
-		plt.ylabel("Nearness [1/m]")
-		plt.title("SF Horiz. Nearness (Array Values)")
+        plt.subplot(4,1,4)
+        # Horizontal Nearness Scan: X [1/m] vs Y [1/m]
+        # plt.figure(num=4, figsize=(9, 6), dpi=80, facecolor='w', edgecolor='k')
+        plt.plot( lastVNearnessScanNP , 'b.' )
+        plt.hold( False )
+        plt.xlim( [ 0 , 720 ] )
+        plt.ylim( [ 0 , 5 ] )
+        plt.xlabel("Index")
+        plt.ylabel("Nearness")
 
-		plt.pause( 0.001 )
-	plt.show()
+
+        plt.pause( 0.01 )
+    plt.show()
 except KeyboardInterrupt:
 	pass
