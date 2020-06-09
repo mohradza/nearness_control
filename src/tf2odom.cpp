0c1339f917ea9b2f642c5485439d989bdcf383cd
mohradza <mioh0887@colorado.edu>
2020-06-08 22:12:33 -0600

    added velocity calculation

--- src/tf2odom.cpp
+++ src/tf2odom.cpp
@@ -24,6 +24,18 @@ int main(int argc, char** argv){
   std::string frame_name;
   nh.param<std::string>("OHRAD_X3", frame_name, "default_value");
   odom_msg.header.frame_id = "world";
+
+  float last_x_pos = 0.0;
+  float last_y_pos = 0.0;
+  float last_z_pos = 0.0;
+
+  float last_x_vel_filt = 0.0;
+  float last_y_vel_filt = 0.0;
+  float last_z_vel_filt = 0.0;
+  float alpha_vel = .95;
+
+  ros::Time last_odom_time = ros::Time::now();
+
   ros::Rate rate(20);
   while (node.ok()){
     tf::StampedTransform transform;
@@ -36,9 +48,17 @@ int main(int argc, char** argv){
       ros::Duration(0.1).sleep();
     }
     odom_msg.header.stamp = ros::Time::now();
+    dt = (odom_msg.header.stamp - last_odom_time).toSec();
+    last_odom_time = odom_msg.header.stamp;
+    
     odom_msg.pose.pose.position.x = transform.getOrigin().x();
     odom_msg.pose.pose.position.y = transform.getOrigin().y();
     odom_msg.pose.pose.position.z = transform.getOrigin().z();
+
+    odom_msg.twist.twist.linear.x = (odom_msg.pose.pose.position.x - last_x_pos)/dt;
+    odom_msg.twist.twist.linear.y = (odom_msg.pose.pose.position.y - last_y_pos)/dt;
+    odom_msg.twist.twist.linear.z = (odom_msg.pose.pose.position.z - last_z_pos)/dt;
+
     tf::quaternionTFToMsg(transform.getRotation().normalize(), odom_msg.pose.pose.orientation);
     robot_odom_pub.publish(odom_msg);
 /*
